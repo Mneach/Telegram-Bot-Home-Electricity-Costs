ac3beca1280439bd2f49d8016f19424d4ccd4335
Mneach <mneachh@gmail.com>
2020-08-21 13:59:56 +0700

    Telegram-Bot-Home-Electricity-Costs V0.2

--- currency format.js
+++ currency format.js
@@ -8,16 +8,12 @@ exports.formatRupiah = (number, prefix, round) => {
       return '';
     }
   
-    if (!_.isNull(round)) {
-      round = Math.pow(10, round || 0);
-      number = Math.ceil(Number(number) / round) * round;
-    }
-  
     if (_.isUndefined(prefix) || _.isNull(prefix)) {
       prefix = 'Rp ';
     }
 
-    return prefix + number.toFixed(0)
+    return prefix + number.toFixed(2)
+      .replace('.',',')
       .toString()
       .replace(thousandSeparatorRegex, '.'); 
   };
